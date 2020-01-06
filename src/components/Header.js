dead18b9b1e5d112c9a2a8caeadab59e913227b2
Minh Tran <mtran@directtechnology.com>
2020-01-05 23:16:08 -0800

    CSS Styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavLink, NavbarBrand } from 'reactstrap';
+import { Navbar, Nav, NavLink, NavbarBrand, Button } from 'reactstrap';
 import { baseUrl } from '../shared/baseUrl';
 
 class Header extends Component {
     render() {
         return (
-            <Navbar variant="light" id="navbar" sticky='top'>
+            <Navbar className="bg-light py-0" variant="light" id="navbar" sticky='top'>
                 <NavbarBrand id="navbarbrand" href="#home"><img src={baseUrl + '/images/bunbohue.png'} width="50" height="50" />  Yummy App</NavbarBrand>
-                <Nav className="mr-auto">
+                <Nav className="mr-auto nav">
                     <NavLink href="#home">Home</NavLink>
                     <NavLink href="#home">Meals</NavLink>
                     <NavLink href="#home">About Us</NavLink>
                     <NavLink href="#home">Contact US</NavLink>
                 </Nav>
+                <Button className="btn-warning">Sign-In</Button>
             </Navbar>
         )
     }
