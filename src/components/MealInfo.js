52b3bd89262963a447028b367b6df4c02ad1bde3
Minh Tran <mtran@directtechnology.com>
2020-01-20 08:36:42 -0800

    Deployment

--- src/components/MealInfo.js
+++ src/components/MealInfo.js
@@ -5,7 +5,7 @@ import { baseUrl } from '../shared/baseUrl';
 function MealInfo(props) {
     return (
         <Card>
-            <CardImg width='100%' src={baseUrl + props.meal.image} alt={props.meal.name} />
+            <CardImg src={baseUrl + props.meal.image} alt={props.meal.name} />
             <CardHeader>{props.meal.name}</CardHeader>
             <CardText>{props.meal.description}</CardText>
         </Card>
--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://localhost:3001/';
+export const baseUrl = 'https://www.react.minhtran.app/src/shared/images/';
